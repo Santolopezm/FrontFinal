31d3dac8c742f95bd7299df1bed3b32dce49a843
Santolopez <luis.lopezm@outlook.com>
2021-10-28 15:27:03 -0500

    creacion de nueva seccion veu, Angelitos

---
+++ src/views/Angelitos.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <v-container>
+      <h1>------------------- Pagina en contruccion ------------------</h1>
+      <p>
+        en este aparte estaran las fotos y los datos de todos los cachorros que
+        podras
+      </p>
+      
+      <p>desarrollado por SantoLopez</p>
+    </v-container>
+  </div>
+</template>
